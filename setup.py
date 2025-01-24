35e815f05bd5095fed84197293569d7c7f435fba
clock zhong <zhong.clock@gmail.com>
2025-01-24 14:54:58 +0800

    added setup.py into project:sat-generator

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# Define project metadata
+setup(
+    name="sat_gen",
+    version="1.0.0",  # Replace with your desired version
+    description="Your SAT formula generation tool",
+    author="cz",
+    author_email="zhongclock@gmail.com",
+    install_requires=[
+        "python-sat",  # Dependency package
+    ],
+)
